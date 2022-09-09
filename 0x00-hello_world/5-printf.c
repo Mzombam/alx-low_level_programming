ab45cace4d3f38a537ef59bea53f0bc2e5882b26
Mzombam <mzombaasante@gmail.com>
2022-09-08 17:23:08 -0700

    My world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,13 +6,9 @@
  * with proper grammar, but the outcome is a piece of art,
  * Return: This function returns 0
  */
-
 int main(void)
-
 {
-
 		printf("with proper grammar, but the outcome is a piece of art,\n");
-
 			return (0);
 }
 Show quoted text
