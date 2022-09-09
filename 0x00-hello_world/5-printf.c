3736aa737ec750f2188f0353f61feadbabebf226
Mzombam <mzombaasante@gmail.com>
2022-09-08 18:32:24 -0700

    fifth

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 /**
@@ -8,8 +7,6 @@
  */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
-Show quoted text
-
