362e7903c9225c4d737360a27e1a8ded1b3b186b
Mzombam <mzombaasante@gmail.com>
2022-09-21 11:01:48 -0700

    my readme file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,32 +1,27 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
- * @dest: string to be copied to
- * @src: to be appended to end of dest
- *
- * Return: returns dest
+ *_strcat-concatenate 2 strings
+ *@dest: first string.
+ *@src: second string.
+ *Return: string.
  */
+
 char *_strcat(char *dest, char *src)
 {
-int i, j;
+int i = 0, j = 0;
 
-i = 0;
-j = 0;
+while (dest[i] != '\0')
+i++;
 
-while (*(dest + i) != '\0')
+while (src[j] != '\0')
 {
+dest[i] = src[j];
 i++;
 j++;
 }
 
-i = 0;
-while (*(src + i) != '\0')
+dest[i] = '\0';
 
-dest[j] = src[i];
-i++;
-j++;
-}
-dest[j] = '\0';
 return (dest);
 }
